5bb652e1190fc94a18b788db8220d55046fb62e7
Anthony Kung <hi@anth.dev>
2021-05-05 23:15:58 -0700

    first commit

--- android/app/src/main/java/com/anthonykung/doorgy/MainApplication.java
+++ android/app/src/main/java/com/anthonykung/doorgy/MainApplication.java
@@ -1,4 +1,4 @@
-package com.doorgy;
+package com.anthonykung.doorgy;
 
 import android.app.Application;
 import android.content.Context;
@@ -6,12 +6,13 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.doorgy.generated.BasePackageList;
+import com.anthonykung.doorgy.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -110,7 +111,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.doorgy.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.anthonykung.doorgy.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
